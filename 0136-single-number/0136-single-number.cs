39e8564abdb054eeb0c6433b32cf6ab8fb9ab825
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2023-09-19 18:15:35 +0530

    Time: 95 ms (71.07%), Space: 43.7 MB (84.41%) - LeetHub

--- 0136-single-number/0136-single-number.cs
+++ 0136-single-number/0136-single-number.cs
@@ -1,12 +1,9 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
-        Array.Sort(nums);
-        int n= nums.Length;
-        for(int i=0;i<n;i+=2){
-            if(i+1==n) return nums[i];
-            if(nums[i]==nums[i+1]) continue;
-            else return nums[i];
+        int res=0;
+        foreach( int x in nums){
+            res^=x;
         }
-        return nums[n-1];
+        return res;
     }
 }
