3549a7049ff19556df2eaabaac31fdd23679c96e
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2023-09-22 17:43:13 +0530

    Time: 3 ms (49.10%), Space: 6.9 MB (10.03%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -5,16 +5,9 @@ public:
         int m=t.length();
         int i=0,j=0;
         while(i<n && j<m){
-            char temp=s[i];
-            while(j<m && t[j]!=temp){
-                j++;
-            }
-            if(j>m) return false;
-            if(t[j]==temp){
-                i++;j++;
-            }
+            if(s[i]==t[j]) i++;
+            j++;
         }
-        if(i<n) return false;
-        return true;
+        return i==n?true:false;
     }
 };
