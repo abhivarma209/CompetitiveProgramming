e2fef4aab207a0b696f50ae172ba5e561055b7d9
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2023-09-27 21:02:47 +0530

    Time: 3 ms (9.11%), Space: 6.4 MB (11.71%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        int n=s.size();
+        string res="";
+        long long len=0;
+        for(auto ch:s){
+            if(ch>='a' && ch<='z') len++;
+            else len*= (ch-'0');
+        }
+        for(int i=n-1;i>=0;i--){
+            if((len==k || k==0 ) && (s[i]>='a' && s[i]<='z')){
+                res+=s[i];
+                return res;
+            }
+            else{
+                if(s[i]>='a' && s[i]<='z'){
+                    len--;
+                }
+                else{
+                    len/=(s[i]-'0');
+                    k%=len;
+                }
+            }
+        }
+        return "";
+    }
+};
