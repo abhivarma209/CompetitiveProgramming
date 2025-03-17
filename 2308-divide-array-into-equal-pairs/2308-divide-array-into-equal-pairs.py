b897badcca2f9809cfec4ea340237342e84043b0
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2025-03-17 23:26:16 +0530

    Time: 2 ms (77.39%), Space: 17.7 MB (91.41%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
@@ -3,7 +3,7 @@ class Solution:
         s = set()
         for num in nums:
             if num in s:
-                s.discard(num)
+                s.remove(num)
             else:
                 s.add(num)
         return True if len(s) == 0 else False
