f365f7ffc2bb4f47dc55fcc83e2be0acaa48f23d
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2025-03-18 18:41:24 +0530

    Time: 39 ms (7.92%) | Memory: 18 MB (90.88%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
@@ -1,18 +1,17 @@
 class Solution:
+    def getCost( s: str, t: str, i: int) -> int:
+        svalue = ord(s[i])
+        tvalue = ord(t[i])
+        cost = abs(svalue-tvalue)
+        return cost
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
         i,j,res,temp=0,0,0,0
-
-        def getCost(i:int) -> int:
-            svalue = ord(s[i])
-            tvalue = ord(t[i])
-            cost = abs(svalue-tvalue)
-            return cost
         
         while i<=j and j<len(s):
-            curr_cost = getCost(j)
+            curr_cost = Solution.getCost(s,t,j)
             temp += curr_cost
             while temp>maxCost:
-                prev_cost = getCost(i)
+                prev_cost = Solution.getCost(s,t,i)
                 temp-=prev_cost
                 i+=1
             res = max(res,j+1-i)
