f3f2b9516d6f4c23788fd9d3595b014a958eef17
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2025-03-18 18:39:38 +0530

    Time: 44 ms (6.54%) | Memory: 18.2 MB (24.10%) - LeetSync

---
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
@@ -0,0 +1,21 @@
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        i,j,res,temp=0,0,0,0
+
+        def getCost(i:int) -> int:
+            svalue = ord(s[i])
+            tvalue = ord(t[i])
+            cost = abs(svalue-tvalue)
+            return cost
+        
+        while i<=j and j<len(s):
+            curr_cost = getCost(j)
+            temp += curr_cost
+            while temp>maxCost:
+                prev_cost = getCost(i)
+                temp-=prev_cost
+                i+=1
+            res = max(res,j+1-i)
+            j+=1
+        
+        return res
