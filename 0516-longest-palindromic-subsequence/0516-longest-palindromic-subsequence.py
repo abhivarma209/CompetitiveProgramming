978e0ef113bdb92c5bbff4ad36b7bfa2ab27554d
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2025-06-20 15:22:58 +0530

    Time: 2371 ms (5.01%), Space: 42.8 MB (40.14%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -5,7 +5,8 @@ class Solution:
         dp = [[0]*(n+1) for _ in range(n+1)]
         for i in range(n):
             for j in range(n):
-                dp[i+1][j+1]=max(dp[i+1][j],dp[i][j+1])
                 if s[i]==t[j]:
-                    dp[i+1][j+1]=max(dp[i][j]+1,dp[i+1][j+1])
+                    dp[i+1][j+1]=max(dp[i][j]+1,dp[i+1][j],dp[i][j+1])
+                else:
+                    dp[i+1][j+1]=max(dp[i+1][j],dp[i][j+1])
         return dp[n][n]
