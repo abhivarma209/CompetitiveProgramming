11121611003581886dd61dd7c8324950c5a15d9e
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2026-05-14 21:50:02 +0530

    Time: 15 ms (10.33%), Space: 19.7 MB (56.63%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,11 +2,9 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         fs= ''
         for ch in s:
-            if ord('A')<=ord(ch)<=ord('Z'):
+            if 'A'<=ch<='Z':
                 asc = ord('a')+ord(ch)-ord('A')
                 ch = chr(asc)
-            if ord('a')<=ord(ch)<=ord('z'):
-                fs+=ch
-            if '0'<=ch<='9':
+            if 'a'<=ch<='z' or '0'<=ch<='9':
                 fs+=ch
         return fs[::-1]==fs
