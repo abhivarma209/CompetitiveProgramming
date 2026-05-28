30a4bd1672730cbde2c47442e2cd1fe5c342730d
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2026-05-28 10:32:29 +0530

    Time: 38 ms (62.36%), Space: 20.3 MB (64.64%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,46 +1,28 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        st = []
-        res,i=0,0
-        while i<len(s):
-            if s[i]==" " or s[i]=="+":
-                i+=1
-            elif s[i]=="(":
-                st.append("(")
-                i+=1
-            elif s[i]==")":
-                temp=0
-                while st:
-                    top=st.pop()
-                    if top=="(":
-                        break
-                    elif type(top)==int:
-                        if st and st[-1]=='-':
-                            top*=(-1)
-                            st.pop()
-                        temp+=top
-                if st and st[-1]=='-':
-                    temp*=-1
-                    st.pop()
-                st.append(temp)
-                i+=1
-            elif '0'<=s[i]<='9':
-                num=""
-                while i<len(s) and '0'<=s[i]<='9':
-                    num+=s[i]
-                    i+=1
-                num = int(num)
-                if st and st[-1]=='-':
-                    st.pop()
-                    num*=(-1)
-                st.append(num)
-            elif s[i]=='-':
-                st.append("-")
-                i+=1
-        while st:
-            el = st.pop()
-            if el=='-':
-                res*=-1
-            elif type(el)==int:
-                res+=el
-        return res
+        res,curr,sign=0,0,1
+        st=[]
+        for ch in s:
+            if '0'<=ch<='9':
+                curr=curr*10+int(ch)
+            elif ch=='+':
+                res+=curr*sign
+                sign = 1
+                curr=0
+            elif ch=='-':
+                res+=curr*sign
+                sign=-1
+                curr=0
+            elif ch=='(':
+                st.append(res)
+                st.append(sign)
+                res=0
+                sign=1
+            elif ch==')':
+                res+=sign*curr
+                curr=0
+                res*=st.pop()
+                res+=st.pop()
+            else:
+                continue
+        return res + sign*curr
