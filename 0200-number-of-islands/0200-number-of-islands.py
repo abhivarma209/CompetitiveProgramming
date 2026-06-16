b3b27046f239f8da8df8fe2f83a0a2b0ef9b867a
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2026-06-16 19:44:01 +0530

    Time: 270 ms (18.5%), Space: 26.5 MB (26.76%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,33 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        columns = len(grid[0])
-        visit = set()
-
-        def bfs(row,col):
-            q = deque()
-            q.append((row,col))
-            visit.add((row,col))
-
-            while q:
-                r,c = q.popleft()
-                directions = [[1,0],[-1,0],[0,1],[0,-1]]
-
-                for dir in directions:
-                    nr,nc = r+dir[0],c+dir[1]
-                    if nr in range(rows) and nc in range(columns) and grid[nr][nc]=="1" and (nr,nc) not in visit:
-                        q.append((nr,nc))
-                        visit.add((nr,nc))
-
-        res = 0
-        for i in range(rows):
-            for j in range(columns):
-                if grid[i][j] == "1" and (i,j) not in visit:
-                    bfs(i,j)
+        vis=set()
+        m,n=len(grid),len(grid[0])
+        di=[[-1,0],[0,-1],[0,1],[1,0]]
+        res=0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]=='1' and (i,j) not in vis:
                     res+=1
-
+                    q=deque([(i,j)])
+                    while q:
+                        x,y=q.popleft()
+                        for dx,dy in di:
+                            nx=x+dx
+                            ny=y+dy
+                            if nx<0 or nx>=m or ny<0 or ny>=n or grid[nx][ny]=='0' or (nx,ny) in vis:
+                                continue
+                            vis.add((nx,ny))
+                            q.append((nx,ny))
         return res
-                        
-
-
-        
+                            
