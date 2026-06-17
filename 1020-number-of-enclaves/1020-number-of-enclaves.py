6fd98d9cec6c2542b8166095e45b4bf1e37d8739
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2026-06-17 23:00:37 +0530

    Time: 167 ms (14.31%), Space: 37.5 MB (29.17%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,30 +1,23 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        vis=set()
         m,n=len(grid),len(grid[0])
-        di=[[-1,0],[0,-1],[0,1],[1,0]]
+        di=[[0,1],[0,-1],[1,0],[-1,0]]
+        def dfs(r,c):
+            if r<0 or r>=m or c<0 or c>=n or grid[r][c]!=1: return
+            grid[r][c]=-1
+            for dr,dc in di:
+                nr,nc=r+dr,c+dc
+                if nr<0 or nr>=m or nc<0 or nc>=n or grid[nr][nc]!=1: continue
+                if grid[nr][nc]==1:
+                    dfs(nr,nc)
+        for i in range(m):
+            for j in range(n):
+                if i in [0,m-1] or j in [0,n-1]:
+                    dfs(i,j)
         res=0
         for i in range(m):
             for j in range(n):
-                if grid[i][j]==1 and (i,j) not in vis:
-                    q=deque([(i,j)])
-                    vis.add((i,j))
-                    isBou=False
-                    if i==0 or j==0 or i==m-1 or j==n-1:
-                        isBou=True
-                    count=0
-                    while q:
-                        x,y=q.popleft()
-                        count+=1
-                        for dx,dy in di:
-                            nx=x+dx
-                            ny=y+dy
-                            if nx<0 or nx>=m or ny<0 or ny>=n or grid[nx][ny]==0 or (nx,ny) in vis:
-                                continue
-                            if nx==0 or ny==0 or nx==m-1 or ny==n-1:
-                                isBou=True
-                            vis.add((nx,ny))
-                            q.append((nx,ny))
-                    if not isBou:
-                        res+=count
+                if grid[i][j]==1:
+                    res+=1
         return res
+
