10395c2d0da0c073028dd5d86ed3089e7da20a65
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2026-06-22 11:59:10 +0530

    Time: 51 ms (51.93%), Space: 19.3 MB (66.18%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -9,4 +9,4 @@ class Solution:
                     dp[i]+=1
                 if not dp[i-num] == 0:
                     dp[i]+=dp[i-num]
-        return 0 if dp[target]==-1 else dp[target]
+        return dp[target]
