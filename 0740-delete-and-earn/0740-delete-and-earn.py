b096d0f9e413d414a2edda7420808ee1b2ccee96
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2026-06-22 11:28:17 +0530

    Time: 0 ms (100%), Space: 20.5 MB (86%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,11 +1,12 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        houses = [0] * 10001
+        n = max(nums)
+        houses = [0] * (n+1)
         for num in nums:
             houses[num]+=num
         first,second=0,houses[0]
         res=houses[0]
-        for i in range(1,10001):
+        for i in range(1,n+1):
             first,second=second,max(first+houses[i],second)
             res=max(res,second)
         return res
