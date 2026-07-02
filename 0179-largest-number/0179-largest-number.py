28dd06855652a3a22898394ca4a35ddbfdb3c87a
abhivarma209 <83779925+abhivarma209@users.noreply.github.com>
2026-07-02 14:46:06 +0530

    Time: 17 ms (8.59%), Space: 19.3 MB (64.11%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -6,11 +6,28 @@ class Solution:
             return -1
         else:
             return 1
-        
-    def largestNumber(self, nums: List[int]) -> str:
+
+    def sorted_approach(self,nums):
         sorted_numbers = sorted(nums,key=cmp_to_key(self.custome_sort_rule))
         res= ""
         for num in sorted_numbers:
+            if len(res)==0 and num==0:
+                continue
+            res+=str(num)
+        return res if res else "0"
+    
+    def larger(self,a,b):
+        return str(a) + str(b) > str(b) + str(a)
+
+    def largestNumber(self, nums: List[int]) -> str:
+        for i in range(1,len(nums)):
+            curr = nums[i]
+            j=i-1
+            while j>=0 and self.larger(curr,nums[j]):
+                nums[j+1],nums[j]=nums[j],nums[j+1]
+                j-=1
+        res= ""
+        for num in nums:
             if len(res)==0 and num==0:
                 continue
             res+=str(num)
